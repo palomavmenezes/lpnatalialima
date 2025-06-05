fdbecfcff4c0ae49064da0b50cab65d34ef2a2c8
palomavmenezes <palomacg.contato@gmail.com>
2025-06-05 13:31:47 -0300

    Adicionando breakpoints para que no mobile mostre 1 depoimento por slide

--- components/elements/testimony.tsx
+++ components/elements/testimony.tsx
@@ -23,10 +23,18 @@ const ContentTestimonies: React.FC<ContentTestimoniesProps> = ({ testimonies }) 
       slidesPerView={2}
       navigation={true}
       pagination={{ clickable: true }}
+      breakpoints={{
+        0: {
+          slidesPerView: 1, // para telas pequenas (mobile)
+        },
+        768: {
+          slidesPerView: 2, // a partir de 768px, mostra 2
+        },
+      }}
     >
       {testimonies.map(({ name, quote }, index) => (
         <SwiperSlide key={index} className="mb-10">
-          <div className="bg-light h-100 p-5">
+          <div className="bg-light h-100 p-5 max-sm:m-5">
             <div className="depoimento">
               <p>{quote}</p>
               <p className="name text-right">- {name}</p>
