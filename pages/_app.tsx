5d4e94bacbcd2562afcc93086e351cc55071df49
palomavmenezes <palomacg.contato@gmail.com>
2025-06-10 19:04:36 -0300

    Fixing deploys errors on vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import "../assets/globals.css";
+import "../assets/home/banner.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Transform from '../components/seasons/Transform';
 import Testimony from '../components/seasons/Testimony'
 import Contact from '../components/seasons/Contact';
 import Footer from '../components/seasons/Footer';
-import "../assets/home/banner.css";
 
 const Home: React.FC = () => {
   return (
